8eef03423968691915056c2b74b2b1b7efd9c01d
StepanKhomyn <skhomyn12@gmail.com>
2024-03-05 22:31:57 +0200

    rr

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    // No need for splitting
+    optimization: {
+      splitChunks: false
+    }
+  }
 })
