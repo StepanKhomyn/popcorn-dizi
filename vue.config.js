434fdbc01cc49400c767035281cfe770db7beca6
Stepan Khomyn <skhomyn@voiptime.net>
2026-06-10 09:34:57 +0300

    Add GitHub Actions CI/CD deployment

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/',
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/popcorn-dizi/"
+      : "/",
   transpileDependencies: true,
-  // css: {
-  //   extract: false,
-  // },
-  // configureWebpack: {
-  //   // No need for splitting
-  //   optimization: {
-  //     splitChunks: false
-  //   }
-  // }
-})
+});
